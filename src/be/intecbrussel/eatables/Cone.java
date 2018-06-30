e3d220167456d19da5872e3bc20f813a746d7444
Injecti0n <rootgebruiker@gmail.com>
2018-06-30 10:41:25 +0200

    updated

--- src/be/intecbrussel/eatables/Cone.java
+++ src/be/intecbrussel/eatables/Cone.java
@@ -4,6 +4,15 @@ public class Cone implements Eatable{
 	
 	Flavor[] balls;
 	
+	public Cone() {
+		super();
+	}
+
+	public Cone(Flavor[] balls) {
+		super();
+		this.balls = balls;
+	}
+
 	public void eat() {
 		
 	}
