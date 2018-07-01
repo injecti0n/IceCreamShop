4c629a3723aa97d03d563d0dcba7944eb5e357aa
Injecti0n <rootgebruiker@gmail.com>
2018-07-01 21:37:23 +0200

    updated

--- src/be/intecbrussel/application/IceCreamApp2.java
+++ src/be/intecbrussel/application/IceCreamApp2.java
@@ -29,11 +29,40 @@ public class IceCreamApp2 {
         };
     	
     	 for (int i = 0; i < eatCar.length; i++) {
-             eatCar[i].eat();
+    		 try {
+				eatCar[i].eat();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("No More Ice To sell... Beat it!!");
+			}
 
          }
          System.out.println(IceShop.getProfit());
     	
-  
+  /*  	PriceList priceList2 = new PriceList(2, 5, 8);
+
+        Stock stock = new Stock(1, 8, 2, 1);
+
+        
+        IceCreamCar<Stock> ice = new IceCreamCar(priceList2, stock);
+
+        Eatable[] eatCar = {
+                iceCar.orderCone(new Flavors[]{Cone.Flavor.CHOCOLATE, Cone.Flavor.BANANA, Cone.Flavor.VANILLA}),
+                iceCar.orderCone(new Flavors[]{Cone.Flavor.VANILLA, Cone.Flavor.VANILLA}),
+                iceCar.orderMagnum(Magnum.MagnumType.ROMANTICSTRAWBERRIES),
+                iceCar.orderMagnum(Magnum.MagnumType.ALPINENUTS),
+                iceCar.orderIceRocket()
+        };
+
+        for (int i = 0; i < eatCar.length; i++) {
+            try {
+                eatCar[i].eat();
+            } catch (NoMoreIceCreamException noMoreIce) {
+                System.out.println("No More Ice To sell... Beat it!!");
+               
+            }
+
+        }
+        System.out.println(iceCar.getProfit());*/
     }
 }
